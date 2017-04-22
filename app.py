bb532a6b697e3b8988482d543638e5240b02d5ec
Efe Oge <efeoge@gmail.com>
2017-04-23 01:02:27 +0300

    try and dont except

--- app.py
+++ app.py
@@ -2,26 +2,28 @@ from eksimsi.models import Entry
 from eksimsi.utils import get_page_number, make_soup, get_subject_title, get_entry_url, create_subject, \
     get_first_subject_url, create_entries_from_a_subject_page, get_paginated_subject_url
 
+try:
+    while Entry.select().where(Entry.is_crawled == False):
 
-while Entry.select().where(Entry.is_crawled == False):
+        entry_id = Entry.select().where(Entry.is_crawled == False)[0].eksi_id
 
-    entry_id = Entry.select().where(Entry.is_crawled == False)[0].eksi_id
+        a_entry_soup = make_soup(get_entry_url(entry_id))
+        if a_entry_soup:
+            subject = create_subject(a_entry_soup)
 
-    a_entry_soup = make_soup(get_entry_url(entry_id))
-    if a_entry_soup:
-        subject = create_subject(a_entry_soup)
+            first_subject_page_soup = make_soup(get_first_subject_url(get_subject_title(a_entry_soup)))
 
-        first_subject_page_soup = make_soup(get_first_subject_url(get_subject_title(a_entry_soup)))
+            created_ids = create_entries_from_a_subject_page(subject, first_subject_page_soup)
 
-        created_ids = create_entries_from_a_subject_page(subject, first_subject_page_soup)
+            page_number = get_page_number(first_subject_page_soup)
+            for page_number in range(2, page_number+1):
+                url = get_paginated_subject_url(first_subject_page_soup, page_number)
+                any_page_soup = make_soup(url)
 
-        page_number = get_page_number(first_subject_page_soup)
-        for page_number in range(2, page_number+1):
-            url = get_paginated_subject_url(first_subject_page_soup, page_number)
-            any_page_soup = make_soup(url)
-
-            created_ids = create_entries_from_a_subject_page(subject, any_page_soup)
-    else:
-        Entry.get(Entry.eksi_id == entry_id).delete_instance()
+                created_ids = create_entries_from_a_subject_page(subject, any_page_soup)
+        else:
+            Entry.get(Entry.eksi_id == entry_id).delete_instance()
+except:
+    pass
 
 print("Finito!")
--- eksimsi/init.py
+++ eksimsi/init.py
@@ -1,5 +1,5 @@
-from eksimsi.constants import MIN_ENTRY_ID, MAX_ENTRY_ID
-from eksimsi.models import Entry
+from .constants import MIN_ENTRY_ID, MAX_ENTRY_ID
+from .models import Entry
 
 for i in range(MIN_ENTRY_ID, MAX_ENTRY_ID):
     print(i)
