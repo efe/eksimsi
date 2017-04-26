54ed47fc0efa2c5d23b8b097af75fb8d29406af3
Efe Oge <efeoge@gmail.com>
2017-04-26 16:28:22 +0300

    import bug is fixed

--- eksimsi/init.py
+++ eksimsi/init.py
@@ -1,5 +1,5 @@
-from .constants import MIN_ENTRY_ID, MAX_ENTRY_ID
-from .models import Entry
+from constants import MIN_ENTRY_ID, MAX_ENTRY_ID
+from models import Entry
 
 for i in range(MIN_ENTRY_ID, MAX_ENTRY_ID):
     print(i)
